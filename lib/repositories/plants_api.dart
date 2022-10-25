f69b2e4e185b63082c311f71636ccbd8814e958f
nardaibarra <narda.ibarra@iteso.mx>
2022-10-25 13:48:19 -0500

    added method to clean api response

--- lib/repositories/plants_api.dart
+++ lib/repositories/plants_api.dart
@@ -1,6 +1,8 @@
+import 'package:green_pulse/Classes/found_plant.dart';
 import 'package:green_pulse/secret/plantsAPI_token.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'dart:convert' as convert;
 
 class PlantsAPI {
   static final PlantsAPI _singleton = PlantsAPI._internal();
@@ -9,7 +11,7 @@ class PlantsAPI {
     return _singleton;
   }
 
-  Future<Response> getPlant(String plant) async {
+  Future<Response> getPlants(String plant) async {
     String token = PLANTS_API_TOKEN;
     Map<String, String> qParams = {'alias': plant};
     var header = {'Authorization': 'Bearer $token'};
@@ -33,6 +35,28 @@ class PlantsAPI {
     print(response.body);
     print(response.statusCode);
     return response;
+  }
+
+  List<FoundPlant> decodeResponse(Response response) {
+    List<FoundPlant> results = [];
+    var info = convert.jsonDecode(response.body);
+    info = info['results'];
+    if (info == null) {
+      return results;
+    }
+
+    try {
+      info.forEach((element) {
+        String pid = element['pid'] ?? '-';
+        String display_pid = element['display_pid'] ?? '-';
+        String alias = element['alias'] ?? '-';
+        results
+            .add(FoundPlant(pid: pid, display_pid: display_pid, alias: alias));
+      });
+    } catch (e) {
+      print(e);
+    }
+    return results;
   }
 
   PlantsAPI._internal();
